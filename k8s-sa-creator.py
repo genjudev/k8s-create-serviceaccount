699bdf63a0f8ebfb95f96f8b9d9f5fb2faeeaeb0
larsonn <lars@feldeisen.de>
2023-11-08 17:08:21 +0100

    Update k8s-sa-creator.py

--- k8s-sa-creator.py
+++ k8s-sa-creator.py
@@ -15,7 +15,7 @@ Usage:
 License:
     MIT License
 
-Copyright (c) 2023 [Your Company or Name]
+Copyright (c) 2023 [Lars Feldeisen]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
